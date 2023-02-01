f487782b5d5189f4dd26222dd14615ec6666d6fe
Duy Chu <duycb92@gmail.com>
2023-02-01 14:19:37 +0700

    fix

--- 01-HelloSpark/HelloSpark.py
+++ 01-HelloSpark/HelloSpark.py
@@ -7,13 +7,16 @@ import time
 if __name__ == "__main__":
     conf = get_spark_app_config()
 
-    spark = SparkSession \
-        .builder \
-        .appName("HelloSpark") \
-        .master("local[2]") \
-        .config(conf=conf) \
-        .config("spark.driver.extraJavaOptions", "-Dlog4j.configuration=file:log4j.properties -Dspark.yarn.app.container.log.dir=app-logs -Dlogfile.name=hello-spark ") \
+    spark = (
+        SparkSession.builder.appName("HelloSpark")
+        .master("local[2]")
+        .config(conf=conf)
+        .config(
+            "spark.driver.extraJavaOptions",
+            "-Dlog4j.configuration=file:log4j.properties -Dspark.yarn.app.container.log.dir=app-logs -Dlogfile.name=hello-spark ",
+        )
         .getOrCreate()
+    )
     # spark.sparkContext.setLogLevel("INFO")
     logger = Log4j(spark)
 
@@ -22,12 +25,15 @@ if __name__ == "__main__":
         sys.exit(-1)
 
     logger.info("Starting HelloSpark")
-
+    conf_out = spark.sparkContext.getConf().getAll()
+    for k, v in conf_out:
+        logger.info(f"{k}={v}")
     survey_raw_df = load_survey_df(spark, sys.argv[1])
     partitioned_survey_df = survey_raw_df.repartition(2)
     count_df = count_by_country(partitioned_survey_df)
-    count_df.show()
-
+    # count_df.show()
+    logger.info(count_df.collect())
+    # input("Enter to exit")
     logger.info("Finished HelloSpark")
     # time.sleep(100)
     spark.stop()
--- 01-HelloSpark/lib/utils.py
+++ 01-HelloSpark/lib/utils.py
@@ -1,23 +1,24 @@
 import configparser
 
 from pyspark import SparkConf
+from pyspark.sql import SparkSession, DataFrame
 
 
-def load_survey_df(spark, data_file):
+def load_survey_df(spark: SparkSession, data_file) -> DataFrame:
     return spark.read \
         .option("header", "true") \
         .option("inferSchema", "true") \
         .csv(data_file)
 
 
-def count_by_country(survey_df):
+def count_by_country(survey_df: DataFrame):
     return survey_df.filter("Age < 40") \
         .select("Age", "Gender", "Country", "state") \
         .groupBy("Country") \
         .count()
 
 
-def get_spark_app_config():
+def get_spark_app_config() -> SparkConf:
     spark_conf = SparkConf()
     config = configparser.ConfigParser()
     config.optionxform = str
