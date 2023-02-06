2e6fb14fa8200f3455f53850de35630eb63a3595
Duy Chu <duycb92@gmail.com>
2023-02-06 11:07:50 +0700

    add notes for 06

--- 06-SparkSQLTableDemo/SparkSQLTableDemo.py
+++ 06-SparkSQLTableDemo/SparkSQLTableDemo.py
@@ -19,8 +19,22 @@ if __name__ == "__main__":
     spark.sql("CREATE DATABASE IF NOT EXISTS AIRLINE_DB")
     spark.catalog.setCurrentDatabase("AIRLINE_DB")
 
-    flightTimeParquetDF.write \
-        .mode("overwrite") \
-        .saveAsTable("flight_data_tbl")
+    # way to store data in internal spark database
+    # spark itself is a database so we can store data in spark
+    # this is spark MANAGED table
+    # supported formats are the same: csv, parquet, json, ... 
+
+    if False:
+        flightTimeParquetDF.write \
+            .mode("overwrite") \
+            .saveAsTable("flight_data_tbl")
+    else:
+        flightTimeParquetDF.write \
+            .format("csv") \
+            .mode("overwrite") \
+            .bucketBy(5, "OP_CARRIER", "ORIGIN") \
+            .sortBy("OP_CARRIER", "ORIGIN") \
+            .saveAsTable("flight_data_tbl")
 
     logger.info(spark.catalog.listTables("AIRLINE_DB"))
+    print(spark.catalog.listTables("AIRLINE_DB"))
